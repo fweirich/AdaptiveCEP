5d107678a19b7ba7ff3d5344c5ba89d895eb86a8
Felix Weirich <felix.weirich@hotmail.de>
2018-10-03 13:25:25 +0200

    annealing unstable bandwidth 1

--- src/main/scala/adaptivecep/distributed/annealing/AppRunnerAnnealing.scala
+++ src/main/scala/adaptivecep/distributed/annealing/AppRunnerAnnealing.scala
@@ -68,10 +68,13 @@ object AppRunnerAnnealing extends App {
       .dropElem1()
       .selfJoin(
         tumblingWindow(1.instances),
-        tumblingWindow(1.instances),
-        frequency > ratio(3.instances, 5.seconds) otherwise { nodeData => println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!") },
-        frequency < ratio(12.instances, 15.seconds) otherwise { nodeData => println(s"PROBLEM:\tNode `${nodeData.name}` emits too many events!") })
-      .and(stream[Float]("C").and(stream[String]("D")), bandwidth > dataRate(40.mbPerSecond) otherwise { nodeData => })
+        tumblingWindow(1.instances)
+        /*frequency > ratio(3.instances, 5.seconds) otherwise { nodeData => println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!") },*/
+        /*frequency < ratio(12.instances, 15.seconds) otherwise { nodeData => println(s"PROBLEM:\tNode `${nodeData.name}` emits too many events!") }*/)
+      .and(stream[Float]("C").and(stream[String]("D")),
+        bandwidth > dataRate(55.mbPerSecond) otherwise { nodeData => }
+        /*latency < timespan(200.milliseconds) otherwise { (nodeData) => /*println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slowly!"*/) }*/)
+
   //AWS Setup
 
   val address1 = Address("akka.tcp", "ClusterSystem", "18.219.222.126", 8000)
