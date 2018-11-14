6fb10d4a77cc9e75d0c55ed0af3244318e6e45f7
Felix Weirich <felix.weirich@hotmail.de>
2018-11-14 18:18:02 +0100

    something 5

--- src/main/scala/adaptivecep/distributed/greedy/AppRunnerGreedy.scala
+++ src/main/scala/adaptivecep/distributed/greedy/AppRunnerGreedy.scala
@@ -69,8 +69,8 @@ object AppRunnerGreedy extends App{
         /*frequency < ratio(12.instances, 15.seconds) otherwise { nodeData => println(s"PROBLEM:\tNode `${nodeData.name}` emits too many events!") }*/)
       .and(stream[Float]("C").and(stream[String]("D")),
         /*bandwidth > dataRate(70.mbPerSecond) otherwise { nodeData => },*/
-        /*latency < timespan(150.milliseconds) otherwise { (nodeData) => /*println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slowly!")*/ }*/
-        frequency > ratio(30.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/ })
+        latency < timespan(150.milliseconds) otherwise { (nodeData) => /*println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slowly!")*/ }
+        /*frequency > ratio(30.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/ }*/)
 
   override def main(args: Array[String]): Unit = {
     if (args.nonEmpty) {
