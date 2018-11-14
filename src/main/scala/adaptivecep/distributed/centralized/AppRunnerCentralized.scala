5bda674c0ad745f4231f27647599f5b1ac5ea9c6
Felix Weirich <felix.weirich@hotmail.de>
2018-11-14 10:38:23 +0100

    different latency threshold

--- src/main/scala/adaptivecep/distributed/centralized/AppRunnerCentralized.scala
+++ src/main/scala/adaptivecep/distributed/centralized/AppRunnerCentralized.scala
@@ -66,7 +66,7 @@ object AppRunnerCentralized extends App {
         /*frequency < ratio(12.instances, 15.seconds) otherwise { nodeData => println(s"PROBLEM:\tNode `${nodeData.name}` emits too many events!") }*/)
       .and(stream[Float]("C").and(stream[String]("D")),
         /*bandwidth > dataRate(70.mbPerSecond) otherwise { nodeData => },*/
-        latency < timespan(100.milliseconds) otherwise { (nodeData) => /*println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slowly!")*/ }
+        latency < timespan(150.milliseconds) otherwise { (nodeData) => /*println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slowly!")*/ }
         /*frequency > ratio(30.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/ }*/)
 
 
