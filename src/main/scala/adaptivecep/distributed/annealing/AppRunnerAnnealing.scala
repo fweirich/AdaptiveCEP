6f5a11ba0b768cfed9f5aa3f9e3058033bd9deb6
Felix Weirich <felix.weirich@hotmail.de>
2018-11-15 11:57:48 +0100

    something 7

--- src/main/scala/adaptivecep/distributed/annealing/AppRunnerAnnealing.scala
+++ src/main/scala/adaptivecep/distributed/annealing/AppRunnerAnnealing.scala
@@ -19,7 +19,7 @@ object AppRunnerAnnealing extends App {
   val config = ConfigFactory.parseFile(file).withFallback(ConfigFactory.load()).resolve()
   var producers: Seq[Operator] = Seq.empty[Operator]
   val r = scala.util.Random
-  var optimizeFor: String = "bandwidth"
+  var optimizeFor: String = "latency"
 
   val actorSystem: ActorSystem = ActorSystem("ClusterSystem", config)
 
@@ -72,9 +72,9 @@ object AppRunnerAnnealing extends App {
         /*frequency > ratio(3.instances, 5.seconds) otherwise { nodeData => println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!") },*/
         /*frequency < ratio(12.instances, 15.seconds) otherwise { nodeData => println(s"PROBLEM:\tNode `${nodeData.name}` emits too many events!") }*/)
       .and(stream[Float]("C").and(stream[String]("D")),
-        /*bandwidth > dataRate(70.mbPerSecond) otherwise { nodeData => },
-        latency < timespan(100.milliseconds) otherwise { (nodeData) => /*println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slowly!")*/ }*/
-        frequency > ratio(500.instances, 1.seconds) otherwise { nodeData => println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!") })
+        /*bandwidth > dataRate(70.mbPerSecond) otherwise { nodeData => },*/
+        latency < timespan(150.milliseconds) otherwise { (nodeData) => /*println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slowly!")*/ }
+        /*frequency > ratio(30.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/ }*/)
 
 
   val query4: Query3[Int, Int, Float] =
