10665a3f76f8cf01db537795f86dc73fa3aee2a8
Felix Weirich <felix.weirich@hotmail.de>
2018-11-16 17:26:54 +0100

    latency 100ms

--- src/main/scala/adaptivecep/distributed/centralized/AppRunnerCentralized.scala
+++ src/main/scala/adaptivecep/distributed/centralized/AppRunnerCentralized.scala
@@ -19,7 +19,7 @@ object AppRunnerCentralized extends App {
   val file = new File("application.conf")
   val config = ConfigFactory.parseFile(file).withFallback(ConfigFactory.load()).resolve()
   var producers: Seq[Operator] = Seq.empty[Operator]
-  var optimizeFor: String = "latencybandwidth"
+  var optimizeFor: String = "latency"
 
   val actorSystem: ActorSystem = ActorSystem("ClusterSystem", config)
 
@@ -66,8 +66,8 @@ object AppRunnerCentralized extends App {
         /*frequency < ratio(12.instances, 15.seconds) otherwise { nodeData => println(s"PROBLEM:\tNode `${nodeData.name}` emits too many events!") }*/)
       .and(stream[Float]("C").and(stream[String]("D")),
         /*bandwidth > dataRate(70.mbPerSecond) otherwise { nodeData => },*/
-        latency < timespan(200.milliseconds) otherwise { (nodeData) => /*println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slowly!")*/ },
-        frequency > ratio(20.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/ })
+        latency < timespan(100.milliseconds) otherwise { (nodeData) => /*println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slowly!")*/ }//,
+        /*frequency > ratio(20.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/ }*/)
 
 
   val address1 = Address("akka.tcp", "ClusterSystem", "18.219.222.126", 8000)
