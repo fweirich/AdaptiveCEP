5f7c471700fffba9ae7989c90edc630cfd6a3f79
Felix Weirich <felix.weirich@hotmail.de>
2018-11-18 14:27:47 +0100

    something 25

--- src/main/scala/adaptivecep/distributed/greedy/AppRunnerGreedy.scala
+++ src/main/scala/adaptivecep/distributed/greedy/AppRunnerGreedy.scala
@@ -20,7 +20,7 @@ object AppRunnerGreedy extends App{
   val config = ConfigFactory.parseFile(file).withFallback(ConfigFactory.load()).resolve()
   var producers: Seq[Operator] = Seq.empty[Operator]
   val r = scala.util.Random
-  var optimizeFor: String = "bandwidth"
+  var optimizeFor: String = "latencybandwidth"
 
   val actorSystem: ActorSystem = ActorSystem("ClusterSystem", config)
   //val consumerHost: ActorRef = actorSystem.actorOf(Props[HostActor], "Host")
@@ -69,8 +69,8 @@ object AppRunnerGreedy extends App{
         /*frequency < ratio(12.instances, 15.seconds) otherwise { nodeData => println(s"PROBLEM:\tNode `${nodeData.name}` emits too many events!") }*/)
       .and(stream[Float]("C").and(stream[String]("D")),
         /*bandwidth > dataRate(70.mbPerSecond) otherwise { nodeData => },*/
-        /*latency < timespan(150.milliseconds) otherwise { (nodeData) => /*println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slowly!")*/ }//,*/
-        frequency > ratio(30.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/ })
+        latency < timespan(200.milliseconds) otherwise { (nodeData) => /*println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slowly!")*/ },
+        frequency > ratio(20.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/ })
 
   override def main(args: Array[String]): Unit = {
     if (args.nonEmpty) {
