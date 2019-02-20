0417fbc0928f8a9a6ce6b93ef4a9ea0ae98da60f
Felix Weirich <felix.weirich@hotmail.de>
2019-02-20 11:19:40 +0100

    reactive interface for decentralized

--- src/main/scala/adaptivecep/distributed/HostActorDecentralizedBase.scala
+++ src/main/scala/adaptivecep/distributed/HostActorDecentralizedBase.scala
@@ -195,7 +195,7 @@ trait HostActorDecentralizedBase extends HostActorBase with System{
       bandwidthResponses += sender()
       sendOutCostMessages()
     case gPE: PlacementEvent => processEvent(gPE, sender())
-    case HostPropsRequest => sender() ! HostPropsResponse(Map(NoHost.asInstanceOf[Host] -> Cost(Duration.apply(3, TimeUnit.DAYS), 0)).withDefaultValue(Cost(Duration.Zero, bandwidth.template.max)))
+    case HostPropsRequest => sender() ! HostPropsResponse(Map(NoHost.asInstanceOf[Host] -> Cost(FiniteDuration(3, TimeUnit.DAYS), 0)).withDefaultValue(Cost(FiniteDuration(0, TimeUnit.SECONDS), bandwidth.template.max)))
     case _ =>
   }
 
