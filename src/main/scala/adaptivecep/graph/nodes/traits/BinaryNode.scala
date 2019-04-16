c81f72d69e0bebd4b8a90c5253db8fae663732f5
Felix Weirich <felix.weirich@hotmail.de>
2019-04-16 14:31:04 +0200

    streamstest

--- src/main/scala/adaptivecep/graph/nodes/traits/BinaryNode.scala
+++ src/main/scala/adaptivecep/graph/nodes/traits/BinaryNode.scala
@@ -104,6 +104,7 @@ trait BinaryNode extends Node {
   }
 
   def emitEvent(event: Event): Unit = {
+    println("emit")
     context.system.scheduler.scheduleOnce(
       FiniteDuration(costs(parentNode).duration.toMillis, TimeUnit.MILLISECONDS),
       () => {
