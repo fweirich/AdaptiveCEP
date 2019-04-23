96a207e6c49eea3b35e073d9a1c45af3c71fc2cc
Felix Weirich <felix.weirich@hotmail.de>
2019-04-23 11:25:03 +0200

    test

--- src/main/scala/adaptivecep/graph/nodes/traits/BinaryNode.scala
+++ src/main/scala/adaptivecep/graph/nodes/traits/BinaryNode.scala
@@ -48,6 +48,7 @@ trait BinaryNode extends Node {
         initialDelay = FiniteDuration(0, TimeUnit.SECONDS),
         interval = FiniteDuration(1000, TimeUnit.MILLISECONDS),
         runnable = () => {
+          println(emittedEvents)
           emittedEvents = 0
         })
     }
--- src/main/scala/adaptivecep/graph/nodes/traits/UnaryNode.scala
+++ src/main/scala/adaptivecep/graph/nodes/traits/UnaryNode.scala
@@ -51,6 +51,7 @@ trait UnaryNode extends Node {
         initialDelay = FiniteDuration(0, TimeUnit.SECONDS),
         interval = FiniteDuration(1000, TimeUnit.MILLISECONDS),
         runnable = () => {
+          println(emittedEvents)
           emittedEvents = 0
         })
     }
