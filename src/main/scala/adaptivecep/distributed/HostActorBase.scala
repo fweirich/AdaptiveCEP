51da247c68b76e41fd47e627aa02e1d492d7a15e
Felix Weirich <felix.weirich@hotmail.de>
2019-05-16 11:53:20 +0200

    test

--- src/main/scala/adaptivecep/distributed/HostActorBase.scala
+++ src/main/scala/adaptivecep/distributed/HostActorBase.scala
@@ -88,7 +88,7 @@ trait HostActorBase extends Actor with ActorLogging with RequiresMessageQueue[Bo
     val template = ContinuousBoundedValue[Double](
       0,
       min = 500, max = 10000,
-      () => (80 - 160 * random.nextDouble, 1 + random.nextInt(10)))
+      () => (160 - 320 * random.nextDouble, 1 + random.nextInt(10)))
 
     def apply() =
       template copy (value = 2000 + 8000 * random.nextDouble)
